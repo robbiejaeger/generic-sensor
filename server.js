4fc7f11d7c615549f288a4b255d78d210c4205f0
Robbie Jaeger <jaeger.rob@gmail.com>
2018-05-30 20:12:07 -0600

    Add 404 redirect

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 
+app.use((request, response, next) => {
+  response.redirect('/');
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Generic Sensor app running on port ${app.get('port')}.`)
 });
