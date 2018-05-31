504505ad8b9b9f98b214e3e7e5033a6d73608822
Robbie Jaeger <jaeger.rob@gmail.com>
2018-05-30 21:27:18 -0600

    Add sensor reading and error boilerplate

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,21 @@
 (() => {
-  console.log('Connected');
+  const initSensors = () => {
+    const accelSensor = new Accelerometer();
+    accelSensor.onreading = updateAccelerationValues;
+    accelSensor.onerror = displayError;
+    accelSensor.start();
+  };
+
+  const updateAccelerationValues = () => {
+    console.log(x, y, z)
+    console.log('Sensor reading.');
+  };
+
+  const displayError = event => {
+    if (event.error.name === 'NotReadableError') {
+      console.log('Sensor is not available.');
+    }
+  };
+
+  initSensors();
 })();
